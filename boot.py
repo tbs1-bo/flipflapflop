0d5ee40f6df409e142e6fc2cf982b045f257b1c9
Marco Bakera <marco@bakera.de>
2018-04-01 22:12:33 +0200

    init.

---
+++ boot.py
@@ -0,0 +1,28 @@
+# ESP8266 startup file
+#
+# This file is executed on every boot (including wake-boot from deepsleep)
+
+#import esp
+#esp.osdebug(None)
+
+import gc
+import time
+import network
+
+#import webrepl
+#webrepl.start()
+gc.collect()
+
+SSID = "esp8266"
+PASSWORD = "micropythoN"
+# Currently setting password does not work
+# sticking to the default password micropythoN
+
+print("waiting some safety seconds before starting")
+time.sleep(3)
+
+print("setup network SSID:", SSID, "Password:", PASSWORD)
+ap = network.WLAN(network.AP_IF)
+ap.active(True)
+ap.config(essid=SSID)  # , password=PASSWORD)
+
