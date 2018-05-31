89daf3e5b5451a17642dd50476c57484b98af3bb
Marco Bakera <bakera@tbs1.de>
2018-05-31 16:02:55 +0200

    cleanup and makeover.

--- rogueflip.py
+++ rogueflip.py
@@ -22,17 +22,16 @@ All coins are yellow
 [255, 255, 0]
 """
 
-import demos
 import time
 import pygame
 
 DEFAULT_WORLD_FILE="ressources/rogueflip_world.pnm"
 
-class Game(demos.DemoBase):  # TODO switch from DemoBase to pygame loop 
+class Game:
     """A roguelike for a flipdot display."""
 
     def __init__(self, flipdotdisplay, worldfile=DEFAULT_WORLD_FILE):
-        super().__init__(flipdotdisplay)
+        self.fdd = flipdotdisplay
         pygame.init()
         self.world = World(worldfile)
         # top left position of current view inside the world
@@ -44,11 +43,19 @@ class Game(demos.DemoBase):  # TODO switch from DemoBase to pygame loop
         self.coins = self.world.find_coins()
         print("Found", len(self.coins), "coins.")
 
+    def run(self):
+        while True:
+            self.prepare()
+            for x in range(self.fdd.width):
+                for y in range(self.fdd.height):
+                    val = self.handle_px(x, y)
+                    self.fdd.px(x, y, val)
+            self.fdd.show()
+
     def handle_px(self, x, y):
         x_, y_ = self.window_top_left[0] + x, self.window_top_left[1] + y
         self.handle_input()
         if self.player.pos == [x_, y_]:
-            # player is blinking
             return self.player.draw()
 
         for coin in self.coins:
@@ -63,7 +70,7 @@ class Game(demos.DemoBase):  # TODO switch from DemoBase to pygame loop
             coin.tick()
 
     def handle_input(self):
-        newx, newy = self.player.pos
+        plx, ply = self.player.pos
         dx, dy = 0, 0
         for event in pygame.event.get():
             if event.type != pygame.KEYDOWN:
@@ -78,8 +85,8 @@ class Game(demos.DemoBase):  # TODO switch from DemoBase to pygame loop
             elif event.key == pygame.K_d:
                 dx = +1
 
-            if not self.world.is_wall(newx+dx, newy+dy):
-                self.player.pos = [newx+dx, newy+dy]
+            if not self.world.is_wall(plx+dx, ply+dy):
+                self.player.pos = [plx + dx, ply + dy]
                 self.player_try_collect_coin()
             if not self.player_in_window():
                 self.move_window(dx, dy)
@@ -118,8 +125,11 @@ class World:
         self.load_world(worldfile)
 
     def load_world(self, file):
-        """Load world from file. The file has to be a PNM-Image file. A wall 
-        is marked with a black, the player with a blue pixel."""
+        """Load world from file. The file has to be a PNM-Image file - format
+        P3, ASCII-based.
+
+        Find the specification of the file format 
+        `here <https://de.wikipedia.org/wiki/Portable_Anymap#Pixmap>`_."""
         print("Loading world", file)
 
         with open(file) as f:
@@ -165,14 +175,14 @@ class World:
         return self.get_px(x, y) == World.COLOR_COIN
 
     def _find_game_objects(self, typ):
-        chars = []
+        gobjs = []
         for x in range(self.width):
             for y in range(self.height):
                 if self.get_px(x, y) == typ:
                     en = GameObject(x, y)
-                    chars.append(en)
+                    gobjs.append(en)
 
-        return chars
+        return gobjs
 
     def find_player(self):
         return self._find_game_objects(World.COLOR_PLAYER)[0]
@@ -189,6 +199,7 @@ class GameObject:
         self.blink_on = False
 
     def tick(self):
+        """A method that should be invoked each frame."""
         if time.time() - self.last_updated > self.blink_interval:
             self.blink_on = not self.blink_on
             self.last_updated = time.time()
@@ -196,3 +207,17 @@ class GameObject:
     def draw(self):
         """Determine whether the character should be drawn now."""
         return self.blink_on
+
+
+def run_simulator():
+    """Run a sample game in the simulator."""
+
+    import flipdotsim
+    fdd = flipdotsim.FlipDotSim(width=28, height=16)
+    g = Game(fdd, DEFAULT_WORLD_FILE)
+    g.run()
+
+
+if __name__ == "__main__":
+    print("running a sample game in the simulator")
+    run_simulator()
