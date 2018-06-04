b82bd6e4c0b5d8a4688638dd3b62c88c57676680
Marco Bakera <bakera@tbs1.de>
2018-06-04 15:52:31 +0200

    added configuration file.

---
+++ configuration_sample.py
@@ -0,0 +1,27 @@
+"""
+Some default configuration options with default valies.
+Copy this file to configuration.py and change the values as needed.
+"""
+
+WIDTH = 28
+HEIGHT = 13
+
+flipdotdisplay = {
+    "i2c_address": 0x20,
+    "modules": [18]
+}
+
+display_server = {
+    "host": "0.0.0.0",
+    "port": 10101,
+    "cooldown_time": 1.0
+}
+
+remote_display = {
+    "host": "0.0.0.0",
+    "port": 10101    
+}
+
+simulator = {
+    "fps": 30
+}
