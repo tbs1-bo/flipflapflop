532d181c1dec6b3e94f1cf2f94db00700cf75c0d
Marco Bakera <marco@bakera.de>
2019-06-17 20:14:00 +0200

    first version for serial interface described in #4

---
+++ fffserial.py
@@ -0,0 +1,25 @@
+import serial  # pip install pyserial
+import displayprovider
+
+DEVICE = '/dev/ttyUSB0'  # TODO move to configuration.py
+
+class SerialDisplay(displayprovider.DisplayBase):
+    DIMENSION = 0b10010000 # Es folgen zwei Bytes mit BREITE und HÖHE.
+    PICTURE = 0b10000001 # Es folgen Breite*Höhe Datenbits (zeilenweise)
+
+    PXSET = 0b10000011  # Es folgen zwei Bytes X, Y mit Positionsinformationen 
+    PXRESET = 0b10000010 # Es folgen zwei Bytes X, y mit Positionsinformationen 
+    ECHO = 0b11110000  # Das gesendet Byte wird zurückgesendet.
+
+    def __init__(self, width=4, height=3):
+        super().__init__(width, height)
+        self.buffer = []
+        self.ser = serial.Serial(DEVICE)
+
+    def px(self, x, y, val):
+        # TODO
+        pass
+
+    def show(self):
+        # TODO
+        pass
