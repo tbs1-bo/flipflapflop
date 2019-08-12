a36b0be4e155fa892dfe7e4bf52b2c5013413aa4
Marco Bakera <marco@bakera.de>
2019-08-12 18:30:17 +0200

    rewrite: switching to tiled maps.

--- presenter.py
+++ presenter.py
@@ -1,39 +1,69 @@
 import displayprovider
 import flipdotfont
+import pytmx
+import flipdotsim
 
-TEXT = [
-    "Hallo",
-    "Welt",
-    "alles",
-    "klar",
-    "soweit",
-    "Sonder",
-    "zeichen",
-    "öäü ÖÄÜ ß"
-]
+WIDTH = 28
+HEIGHT = 13
+presentation_file = 'ressources/rogueflip_world.tmx'
+fdd = flipdotsim.FlipDotSim(width=WIDTH, height=HEIGHT)
 
+tiled_map = None
+offsetx, offsety = 0, 0
 
 def main():
-    print("Starting Presenter")
-    current = 0
-    fdd = displayprovider.get_display()
-    scroller = flipdotfont.TextScroller(fdd)
-    font = flipdotfont.big_font()
-    
+    global tiled_map, offsetx, offsety
+    tiled_map = pytmx.TiledMap(presentation_file)
+
     while True:
-        fdd.clear()
-        txt = TEXT[current]
-        scroller.text(txt, font)
-        # FIXME Error when using y value different from 0
-        #scroller.text(bottom, font, start=(0, font.height + 1))
-        nexti = (current + 1) % len(TEXT)
-        current = nexti
-        cmd = input(str(current) + " " + TEXT[current] + " Enter for next, gXX for goto ")
-        
-        if cmd == 'q':
+        display(offsetx, offsety)
+        s = input("> ")
+        handle_input(s)
+
+
+def handle_input(userin):
+    global offsetx, offsety
+
+    if userin == 'a' and on_map(offsetx - WIDTH, 0):
+        offsetx -= WIDTH
+    elif userin == 'd' and on_map(offsetx + WIDTH, 0):
+        offsetx += WIDTH
+    elif userin == 'w' and on_map(0, offsety - HEIGHT):
+        offsety -= HEIGHT
+    elif userin == 's' and on_map(0, offsety + HEIGHT):
+        offsety += HEIGHT
+    elif ',' in userin:
+        inx, iny = userin.split(',')
+        inx, iny = int(inx), int(iny)
+        if on_map(inx, iny):
+            offsetx, offsety = inx, iny
+    elif userin == 'q':
             exit()
-        elif cmd.startswith('g'):
-            current = int(cmd[1:])
+
+    
+def on_map(x, y):
+    'check if the coordinates are on the tiled map.'
+    return 0 <= x < tiled_map.width and \
+           0 <= y < tiled_map.height
+
+
+def display(offsetx, offsety):
+    print("drawing@", offsetx, offsety)
+    disp = ''
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            x_ = offsetx + x
+            y_ = offsety + y
+            if on_map(x_, y_):
+                # id 0: black, id 1: yellow
+                yellow = tiled_map.get_tile_properties(
+                    x_,y_,0)['id'] == 1
+                fdd.px(x,y,yellow)
+                disp += '*' if yellow else '.'
+        disp += '\n'
+
+    print(disp)
+    fdd.show()
 
 
 if __name__ == '__main__':
