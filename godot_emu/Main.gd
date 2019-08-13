45ddaea12e1c150c9458da41fa2dceba771f8a98
Marco Bakera <marco@bakera.de>
2019-08-13 08:01:57 +0200

    map can be moved with cursor keys.

--- godot_emu/Main.gd
+++ godot_emu/Main.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
+var map: TileMap
+const STEP_SIZE = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print('starting')
+	map = get_node("Map/TileMap")
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_pressed('ui_left'):
+		#print('left')
+		map.position.x -= STEP_SIZE
+	if Input.is_action_pressed('ui_right'):
+		#print('right')
+		map.position.x += STEP_SIZE
+	if Input.is_action_pressed('ui_up'):
+		map.position.y -= STEP_SIZE
+	if Input.is_action_pressed('ui_down'):
+		map.position.y += STEP_SIZE
