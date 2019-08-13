cab94667b3406dd6f2cdf84a2ed5f9e6a6942890
Marco Bakera <marco@bakera.de>
2019-08-13 08:15:14 +0200

    cleanup comments.

--- godot_emu/Main.gd
+++ godot_emu/Main.gd
@@ -12,16 +12,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed('ui_left'):
-		#print('left')
 		map.position.x -= STEP_SIZE
 	if Input.is_action_pressed('ui_right'):
-		#print('right')
 		map.position.x += STEP_SIZE
 	if Input.is_action_pressed('ui_up'):
 		map.position.y -= STEP_SIZE
 	if Input.is_action_pressed('ui_down'):
 		map.position.y += STEP_SIZE
-		
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
