0fd03d11416cef6cc4e9290cac43e65b883d120e
Marco Bakera <marco@bakera.de>
2019-08-13 22:53:04 +0200

    change dot by click.

--- godot_client/fff.gd
+++ godot_client/fff.gd
@@ -1,19 +1,31 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var peer: StreamPeerTCP
+const DEFAULT_PORT = 10101
+
+func connect_to(host):
+	print("connect to %s" % host)
+	peer = StreamPeerTCP.new()
+	peer.connect_to_host(host, DEFAULT_PORT)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _input(event):
+	if not self.visible:
+		return
+		
 	if event is InputEventMouse:
 		#print(event)
 		var mousepos = event.position
 		var mappos = $TileMap.world_to_map(mousepos)
 		print("world %s \t map %s" % [mousepos, mappos])
+	
+	if event is InputEventMouseButton and event.pressed:
+		var tile_pos = $TileMap.world_to_map(event.position)
+		var old_tile = $TileMap.get_cellv(tile_pos)
+		$TileMap.set_cellv(tile_pos, 1-old_tile)
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
