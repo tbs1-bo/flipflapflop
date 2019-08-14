dd100104fd9f384aebce8321ee4313e29dbbd9a3
Marco Bakera <marco@bakera.de>
2019-08-14 07:59:11 +0200

    code cleanup

--- godot_client/fff.gd
+++ godot_client/fff.gd
@@ -3,6 +3,7 @@ extends Node2D
 var peer: StreamPeerTCP
 const DEFAULT_PORT = 10101
 var display_size
+var tmap
 
 func connect_to(host):
 	print("connect to %s on port %s" % [host, DEFAULT_PORT])
@@ -13,12 +14,12 @@ func connect_to(host):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	tmap = $TileMap
 
 func init_display(width, height):
 	for x in range(width):
 		for y in range(height):
-			$TileMap.set_cell(x, y, 0)
+			tmap.set_cell(x, y, 0)
 
 	display_size = Vector2(width, height)
 
@@ -27,9 +28,9 @@ func _input(event):
 		return
 		
 	if event is InputEventMouseButton and event.pressed:
-		var tile_pos = $TileMap.world_to_map(event.position)
-		var old_tile = $TileMap.get_cellv(tile_pos)
-		$TileMap.set_cellv(tile_pos, 1-old_tile)
+		var tile_pos = tmap.world_to_map(event.position)
+		var old_tile = tmap.get_cellv(tile_pos)
+		tmap.set_cellv(tile_pos, 1-old_tile)
 		send_to_server()
 
 func send_to_server():
