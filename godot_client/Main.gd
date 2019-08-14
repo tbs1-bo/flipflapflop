a4068bb3f5c758c4fbba37fbb780ce33e349a045
Marco Bakera <marco@bakera.de>
2019-08-14 07:43:44 +0200

    switching to flexible config.

--- godot_client/Main.gd
+++ godot_client/Main.gd
@@ -1,20 +1,26 @@
 extends Control
 
 var fff
-var config_panel
+var line_edit_ip
+var line_edit_width
+var line_edit_height
+var config
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	fff = $fff
-	config_panel = $PanelConfig
+	line_edit_ip = $MarginContainer/VBoxContainer/HBoxContainerConnection/LineEditIpAdress
+	line_edit_width = $MarginContainer/VBoxContainer/HBoxContainerSize/LineEditWidth
+	line_edit_height = $MarginContainer/VBoxContainer/HBoxContainerSize/LineEditHeight
+	config = $MarginContainer
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func _on_ButtonConnect_pressed():
-	config_panel.hide()
-	fff.connect_to($PanelConfig/LineEditIpAdress.text)
-	fff.init_display(int($PanelConfig/LineEditWidth.text),
-					  int($PanelConfig/LineEditHeight.text))
+	config.hide()
+	fff.connect_to(line_edit_ip.text)
+	fff.init_display(int(line_edit_width.text),
+					  int(line_edit_height.text))
 	fff.visible = true
