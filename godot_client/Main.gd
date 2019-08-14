e9081ac0aff5124067c0ac2c71c3b4935cdb4753
Marco Bakera <marco@bakera.de>
2019-08-14 07:02:46 +0200

    using variable for components.

--- godot_client/Main.gd
+++ godot_client/Main.gd
@@ -1,16 +1,20 @@
 extends Control
 
+var fff
+var config_panel
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	fff = $fff
+	config_panel = $PanelConfig
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func _on_ButtonConnect_pressed():
-	$PanelConfig.hide()
-	$fff.connect_to($PanelConfig/LineEditIpAdress.text)
-	$fff.init_display(int($PanelConfig/LineEditWidth.text),
+	config_panel.hide()
+	fff.connect_to($PanelConfig/LineEditIpAdress.text)
+	fff.init_display(int($PanelConfig/LineEditWidth.text),
 					  int($PanelConfig/LineEditHeight.text))
-	$fff.visible = true
+	fff.visible = true
