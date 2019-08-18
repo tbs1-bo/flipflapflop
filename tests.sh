8ee59a7a59b629ee34b0cebb79c48dad48965f9a
Marco Bakera <marco@bakera.de>
2019-08-18 22:36:36 +0200

    moved tests file, added pytests.

--- tests.sh
+++ tests.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# running doctests
 FILES="flipdotsim.py flipdotfont.py displayprovider.py net.py rogueflip.py"
 echo testing $FILES
 python3 -m doctest $FILES
+
+# running tests
+FILES='fffmqtt.py'
+pytest -v $FILES
