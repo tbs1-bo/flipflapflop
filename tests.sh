0bdcbae81c2e70606e04e812aae5ae0f0d485bf3
Marco Bakera <marco@bakera.de>
2024-12-22 10:28:27 +0100

    disable fffmqtt.py in tests due to timeout issues with public mqtt test broker

--- tests.sh
+++ tests.sh
@@ -3,7 +3,8 @@
 make configuration.py
 
 # running doctests
-FILES="flipdotsim.py flipdotfont.py displayprovider.py net.py rogueflip.py fffmqtt.py"
+FILES="flipdotsim.py flipdotfont.py displayprovider.py net.py rogueflip.py"
+# Bakera, 2024-12-22: Disabling fffmqtt.py because of timeout issues with public mqtt test broker
 echo testing $FILES
 # turn off pygame greeting upon first import
 PYGAME_HIDE_SUPPORT_PROMPT=1
@@ -12,7 +13,9 @@ poetry run python -m doctest $FILES
 # running tests using pytest
 ls *py | \
     # ignoring some files for tests because of missing dependencies
+    # Bakera, 2024-12-22: Disabled fffmqtt.py because of timeout issues with public mqtt test broker
     grep -v displayserver_service.py | \
     grep -v flipdotdisplay.py | \
     grep -v MCP23017.py | \
+    grep -v fffmqtt.py | \ 
     xargs poetry run pytest -v 
