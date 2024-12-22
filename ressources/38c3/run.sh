d64b7492d62533336d26af07121851c4c2acb922
Marco Bakera <marco@bakera.de>
2024-12-22 16:21:36 +0100

    rogueflip: log output in run script

--- ressources/38c3/run.sh
+++ ressources/38c3/run.sh
@@ -3,6 +3,10 @@
 export ROGUEFLIP_WORLD_FILE=ressources/38c3/rogueflip_38c3.tmx
 export ROGUEFLIP_WIN_MESSAGE="you win the badge 123 456 789"
 
+CURENT_DIR=$(pwd)
+
 # starting rogueflip
 cd ../..
-python rogueflip.py
+# start rogueflip and log output
+python -u rogueflip.py | tee -a $CURENT_DIR/rogueflip.log
+
