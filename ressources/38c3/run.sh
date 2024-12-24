4cf2fd88311991fbfab56a5cd6f53b70e58176d8
Marco Bakera <marco@bakera.de>
2024-12-24 10:13:19 +0100

    38c3: big level file

--- ressources/38c3/run.sh
+++ ressources/38c3/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export ROGUEFLIP_WORLD_FILE=ressources/38c3/rogueflip_38c3.tmx
+export ROGUEFLIP_WORLD_FILE=ressources/38c3/rogueflip_38c3_medium.tmx
 export ROGUEFLIP_WIN_MESSAGE="you win the badge 123 456 789"
 
 CURENT_DIR=$(pwd)
