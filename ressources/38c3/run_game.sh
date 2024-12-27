179080f6d596881b684e028842a89ed7866d47ff
Pintman <pintman@0xabc.de>
2024-12-27 11:19:47 +0100

    runner updated

--- ressources/38c3/run_game.sh
+++ ressources/38c3/run_game.sh
@@ -2,11 +2,12 @@
 
 export ROGUEFLIP_WORLD_FILE=ressources/38c3/rogueflip_38c3_medium.tmx
 export ROGUEFLIP_WIN_MESSAGE="you win the badge 123 456 789"
+export SDL_VIDEODRIVER=dummy
 
 CURENT_DIR=$(pwd)
 
 # starting rogueflip
 cd ../..
 # start rogueflip and log output
-python -u rogueflip.py 2>&1 | tee -a $CURENT_DIR/rogueflip.log
+python3 -u rogueflip_38c3.py 2>&1 | tee -a $CURENT_DIR/rogueflip.log
 
