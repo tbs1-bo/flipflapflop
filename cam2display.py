2784f4f3f71e3514dd9797331cc26f9357685baa
Pintman <pintman@0xabc.de>
2024-12-30 12:07:28 +0100

    refactor cam2display to use App class and blur

--- cam2display.py
+++ cam2display.py
@@ -1,57 +1,59 @@
 import cv2 # pip install opencv-python
 import configuration
 import displayprovider
+import pyxel
 
 # Open webcam
 
-def main():
-    cap = cv2.VideoCapture(0)
-    fdd = displayprovider.get_display()
-    
-    while True :
+class App:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+        self.fdd = displayprovider.get_display()
+        pyxel.init(configuration.WIDTH, configuration.HEIGHT, fps=60)
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        ...
+
+    def draw(self):        
         # Capture frame-by-frame
-        ret, frame = cap.read()
+        ret, frame = self.cap.read()
+
+        # flip frame vertically
+        frame = cv2.flip(frame, 1)
 
         # Check if frame is captured successfully
         if not ret:
             print("Failed to capture frame")
-            break
 
         resized_frame = cv2.resize(frame, (configuration.WIDTH, configuration.HEIGHT))
-
-        # Crop the frame
-        #height, width = frame.shape[:2]
-        #start_x = 0
-        #start_y = 0
-        #end_x = start_x + configuration.WIDTH
-        #end_y = start_y + configuration.HEIGHT
-        #cropped_frame = frame[start_y:end_y, start_x:end_x]
 
         # Convert the frame to grayscale
         gray_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
 
+        # Apply Gaussian blur
+        gray_frame = cv2.GaussianBlur(gray_frame, (5, 5), 0)
+
         # Apply binary thresholding
-        _, bw_frame = cv2.threshold(gray_frame, 100, 255, cv2.THRESH_BINARY)
+        _, bw_frame = cv2.threshold(gray_frame, 127, 255, cv2.THRESH_BINARY)
 
         # Iterate over each pixel and send to filipdotdisplay
-        fdd.clear()
+        self.fdd.clear()
+        pyxel.cls(0)
         for y in range(configuration.HEIGHT):
             for x in range(configuration.WIDTH):
                 pixel_value = bw_frame[y, x]
-                fdd.px(x, y, pixel_value != 0)
-        fdd.show()
+                self.fdd.px(x, y, pixel_value != 0)
+                pyxel.pset(x, y, 7 if pixel_value != 0 else 0)
+        self.fdd.show()
 
         # Display the resulting frame
         cv2.imshow('Webcam', gray_frame)
 
-        # Break the loop on 'q' key press
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # Release the capture and close windows
-    cap.release()
-    cv2.destroyAllWindows()
+        # Release the capture and close windows
+        #cap.release()
+        #cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    main()
+    App()
