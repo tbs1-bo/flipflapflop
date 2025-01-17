5162e5ff68c1ea923d00def0db84133e8e177b5c
Marco Bakera <marco@bakera.de>
2025-01-17 17:44:31 +0100

    feat: update OpenWRT deployment script to use file downloads instead of git clone

--- deployment/deploy_openwrt.sh
+++ deployment/deploy_openwrt.sh
@@ -7,8 +7,10 @@ echo "Installing packages"
 opkg update
 opkg install python3-light python3-pyserial nano
 
+# using download instead of git clone to save space
+
 echo "Download files from GitHub"
-FILES="fffserial.py binclock.py displayprovider.py net.py configuration_sample.py displayserver_service.py deployment/openwrt_service.sh"
+FILES="fffserial.py binclock.py displayprovider.py net.py configuration_sample.py displayserver_service.py deployment/openwrt_service.sh demos.py"
 BASE_URL="https://raw.githubusercontent.com/tbs1-bo/flipflapflop/refs/heads/master/"
 mkdir -p flipflapflop/deployment
 for f in $FILES; do wget $BASE_URL/$f -O flipflapflop/$f; done
