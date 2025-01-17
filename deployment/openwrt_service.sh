e6a3ad7b4c9aba9e9a9588f30794822e2e1778ef
Marco Bakera <marco@bakera.de>
2025-01-17 16:52:03 +0100

    feat: add OpenWRT service script for display server initialization

---
+++ deployment/openwrt_service.sh
@@ -0,0 +1,21 @@
+#!/bin/sh /etc/rc.common
+
+# Running service script for OpenWRT
+# https://openwrt.org/docs/guide-developer/procd-init-script-example
+
+# must be copied to /etc/init.d/flipflapflop
+# and enabled with /etc/init.d/flipflapflop enable
+
+# TODO must be tested
+
+START=99
+USE_PROCD=1
+
+start_service() {
+    procd_open_instance
+    #procd_set_param command /usr/bin/python -u /root/flipflapflop/displayserver_service.py
+    procd_set_param command /bin/sh -c 'cd /root/flipflapflop && /usr/bin/python -u displayserver_service.py'
+    procd_set_param stdout 1
+    procd_set_param stderr 1
+    procd_close_instance
+}
