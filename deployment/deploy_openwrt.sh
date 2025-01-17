02c21d4c9384dcc988571e3e423bf596b3516582
Marco Bakera <marco@bakera.de>
2025-01-17 16:31:54 +0100

    feat: rename and enhance deployment script for OpenWRT installation

--- deployment/deploy_openwrt.sh
+++ deployment/deploy_openwrt.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
+#
+# Install script for OpenWRT - e.g. running on an Orange Pi Zero.
+#
+
+echo "Installing packages"
+opkg update
+opkg install python3-light python3-pyserial git
 
 echo "Cloning the repository with a depth of 1"
 git clone --depth 1 https://github.com/tbs1-bo/flipflapflop
-
 
 # Alternative: Download the repository as a zip file
 #wget -v https://github.com/tbs1-bo/flipflapflop/archive/refs/heads/master.zip
 #unzip master.zip
 #mv -v flipflapflop-master flipflapflop
 #rm -v master.zip
+
+echo "copy default configuration"
+cp -v flipflapflop/configuration_sample.py flipflapflop/configuration.py
