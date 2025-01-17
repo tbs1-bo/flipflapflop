62d3928c5c5f47bd5d8df7a875431831b8352b90
Marco Bakera <marco@bakera.de>
2025-01-17 23:04:55 +0100

    feat: update file download list in OpenWRT deployment script

--- deployment/deploy_openwrt.sh
+++ deployment/deploy_openwrt.sh
@@ -10,7 +10,9 @@ opkg install python3-light python3-pyserial nano
 # using download instead of git clone to save space
 
 echo "Download files from GitHub"
-FILES="fffserial.py binclock.py displayprovider.py net.py configuration_sample.py displayserver_service.py deployment/openwrt_service.sh demos.py"
+FILES="fffserial.py binclock.py displayprovider.py net.py \
+configuration_sample.py displayserver_service.py demos.py \
+deployment/openwrt_service.sh"
 BASE_URL="https://raw.githubusercontent.com/tbs1-bo/flipflapflop/refs/heads/master/"
 mkdir -p flipflapflop/deployment
 for f in $FILES; do wget $BASE_URL/$f -O flipflapflop/$f; done
