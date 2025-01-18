4fd33fa60ff3e173c9af64d4ee9cbe34f1e04c29
Marco Bakera <marco@bakera.de>
2025-01-18 17:30:46 +0100

    doc gen

--- docs/_downloads/6a57827c233b4c00be6ee51855ed53d7/deploy_openwrt.sh
+++ docs/_downloads/6a57827c233b4c00be6ee51855ed53d7/deploy_openwrt.sh
@@ -2,15 +2,26 @@
 #
 # Install script for OpenWRT - e.g. running on an Orange Pi Zero.
 #
+# There is a firmware selector that allows to define included packages
+# https://firmware-selector.openwrt.org/
+#
+# DHCP client mode could be setup there as well:
+# uci set network.lan.proto="dhcp"
+# uci commit network
+#
+# https://openwrt.org/docs/guide-user/network/openwrt_as_clientdevice#command-line_instructions
 
 echo "Installing packages"
 opkg update
 opkg install python3-light python3-pyserial nano
 
+
 # using download instead of git clone to save space
 
 echo "Download files from GitHub"
-FILES="fffserial.py binclock.py displayprovider.py net.py configuration_sample.py displayserver_service.py deployment/openwrt_service.sh demos.py"
+FILES="fffserial.py binclock.py displayprovider.py net.py \
+configuration_sample.py displayserver_service.py demos.py \
+deployment/openwrt_service.sh"
 BASE_URL="https://raw.githubusercontent.com/tbs1-bo/flipflapflop/refs/heads/master/"
 mkdir -p flipflapflop/deployment
 for f in $FILES; do wget $BASE_URL/$f -O flipflapflop/$f; done
